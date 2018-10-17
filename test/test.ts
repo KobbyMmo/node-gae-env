64046c71480ad81e7273fcf1a9b5724e5d94d102
Obed <obedamoasi@gmail.com>
2018-10-17 12:42:55 +0000

    init

---
+++ test/test.ts
@@ -0,0 +1,9 @@
+import {getWordFromNumber} from '../src';
+import { expect } from 'chai';
+import 'mocha';
+
+// describe('store env in GAE', () => {
+//   it('store xyz in gae', () => {
+//   });
+
+});
